2d0687fa0ae7e4b1d3f8362a6f0ebe4237580891
Caio Carvalho <k4io12345@gmail.com>
2025-10-15 00:46:02 -0300

    - vite logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
